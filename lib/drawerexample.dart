573ccda7df786e339354a3c41af0ddbf42554576
Shashank25071999 <sahaishashank2507@gmail.com>
2020-05-29 01:01:27 +0530

    drawer working

---
+++ lib/drawerexample.dart
@@ -0,0 +1,56 @@
+import 'package:drawer/drawer.dart';
+import 'package:flutter/material.dart';
+
+
+class DrawerExample extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return DrawerExampleState();
+    throw UnimplementedError();
+  }
+}
+
+class DrawerExampleState extends State<DrawerExample>{
+
+    List<DrawerItem> listpages = [
+        
+        DrawerItem(
+          icon: Icon(Icons.library_books),
+          label:"News"
+        ),
+        DrawerItem(
+          icon: Icon(Icons.calendar_today),
+          label: "Schedule"
+        ),
+         DrawerItem(
+          icon: Icon(Icons.home),
+          label:"Home"
+        ),
+         DrawerItem(
+          icon: Icon(Icons.person_add),
+          label:"Register"
+        ),
+        DrawerItem(
+          icon: Icon(Icons.people),
+          label:"About Us"
+        ),
+
+    
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(appBar: AppBar(
+                  title: Text("AppBar"),
+                  leading: Builder(
+        builder: (context) => IconButton(
+            icon: new Icon(Icons.settings),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
+                ),
+                drawer: Stack(children:[ CurvedDrawer(items: listpages)]),
+                body: Container(child: Center(child: Text("data"),),),);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+// import 'package:curved_drawer/curved_drawer.dart';
+import 'package:drawer/drawerexample.dart';
+import 'package:flutter/material.dart';
+import 'package:drawer/drawer.dart';
+
+
+void main() {
+  List<DrawerItem>items=[DrawerItem(icon: Icon(Icons.home),label: "Riya Garg"),
+ DrawerItem(icon: Icon(Icons.hot_tub),label: "Shashank"),
+ DrawerItem(icon: Icon(Icons.hotel),label: "Anshul Sahai"),
+ DrawerItem(icon: Icon(Icons.http),label: "Appu Garg"),
+ DrawerItem(icon: Icon(Icons.image),label: "Nautanki sala")];
+  runApp(MaterialApp(home:DrawerExample()));
+}
+
